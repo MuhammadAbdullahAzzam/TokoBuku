7f0809af3705033cdf5243b7058a9f6e21f06b85
Muhammad Abdullah Azzam <79858774+MuhammadAbdullahAzzam@users.noreply.github.com>
2021-07-10 19:23:08 +0700

    First Commit

--- home/+koneksi.php
+++ home/+koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$nama = "root";
-$pass = "";
-$db = "db_toko";
+$nama = "ucp2pkw_MuhammadAbdullahAzzam";
+$pass = "ucp2pkw_MuhammadAbdullahAzzam";
+$db = "ucp2pkw_TokoBuku";
 
 $koneksi = mysqli_connect($host, $nama, $pass, $db);
 ?>
